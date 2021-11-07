0277c073895691da4dad51e411ed5d6dacd6b149
Charlie3169 <77686049+Charlie3169@users.noreply.github.com>
2021-11-07 13:02:57 -0500

    Added starter code

--- scene.js
+++ scene.js
@@ -1,6 +1,10 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.134.0';
 //import * as THREE from 'three';
 
+//Starter Three.js code
+//Have been unable to get running due to problems with importing 
+//Will keeping messing around with it
+
 const FOV = 75;
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -24,3 +28,86 @@ function animate() {
 	renderer.render(scene, camera);    
 }
 animate();
+
+
+
+
+const arenaXSize = 100;
+const arenaYSize = 100;
+const arenaZSize = 100;
+
+function Circle(x, y, z, dx, dy, dz, radius) {
+    
+    this.x = x
+    this.y = y
+	this.z = z
+    this.dx = dx
+    this.dy = dy
+	this.dz = dz   
+    
+    this.radius = radius
+    
+
+    this.draw = function () {
+        
+    }         
+    
+    this.collisionDetection = function (circle) {
+                           
+    }   
+    
+
+    this.update = function () {      
+        
+        this.x = (this.x + this.dx) % arenaXSize
+        this.y = (this.y + this.dy) % arenaYSize
+		this.z = (this.z + this.dz) % arenaZSize
+
+        if(this.x < 0)        
+            this.x = arenaXSize;        
+
+        if(this.y < 0)        
+            this.y = arenaYSize;      
+
+		if(this.z < 0)        
+            this.z = arenaZSize;      
+                                                           
+        
+        this.draw()                           
+    }
+
+}
+
+
+document.addEventListener('keydown', function(event) {
+
+    if(event.code == 'ShiftLeft'){}	
+        //Player z down
+    
+    if(event.code == 'Space'){}	  
+        //Player z up
+    
+
+    if(event.code == 'KeyW'){}	       
+        //Player x forward
+    
+
+    if(event.code == 'KeyS'){}	
+		//Player x backward
+    
+	if(event.code == 'KeyA'){}	         
+        //Player y left
+
+	if(event.code == 'KeyD'){}	          
+        //Player y right
+        
+})
+
+
+function Player(x, y, z, dx, dy, dz, ) {
+
+
+}
+
+
+
