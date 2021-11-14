e6beac7cfad94763a1d45fa84e917cfc2991878b
Charlie3169 <77686049+Charlie3169@users.noreply.github.com>
2021-11-14 17:57:10 -0500

    Got original code to work

--- src/entry.js
+++ src/entry.js
@@ -1,12 +1,4 @@
-/**
- * entry.js
- * 
- * This is the first file loaded. It sets up the Renderer, 
- * Scene and Camera. It also starts the render loop and 
- * handles window resizes.
- * 
- */
-
+/*
 import { WebGLRenderer, PerspectiveCamera, Scene, Vector3 } from 'three';
 import SeedScene from './objects/Scene.js';
 
@@ -47,3 +39,117 @@ window.addEventListener('resize', windowResizeHanlder);
 // dom
 document.body.style.margin = 0;
 document.body.appendChild( renderer.domElement );
+*/
+
+import * as THREE from 'three';
+
+
+const FOV = 75;
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(FOV, width / height, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x27ccbb} );
+const box = new THREE.Mesh( geometry, material );
+scene.add(box);
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame(animate);
+  box.rotateX(0.01);
+  box.rotateY(0.01);  
+	renderer.render(scene, camera);    
+}
+animate();
+
+
+
+
+const arenaXSize = 100;
+const arenaYSize = 100;
+const arenaZSize = 100;
+
+function Circle(x, y, z, dx, dy, dz, radius) {
+    
+    this.x = x
+    this.y = y
+    this.z = z
+    this.dx = dx
+    this.dy = dy
+	this.dz = dz   
+    
+    this.radius = radius
+    
+
+    this.draw = function () {
+        
+    }         
+    
+    this.collisionDetection = function (circle) {
+                           
+    }   
+    
+
+    this.update = function () 
+    {      
+        
+        this.x = (this.x + this.dx) % arenaXSize
+        this.y = (this.y + this.dy) % arenaYSize
+		    this.z = (this.z + this.dz) % arenaZSize
+
+        if(this.x < 0)        
+            this.x = arenaXSize;        
+
+        if(this.y < 0)        
+            this.y = arenaYSize;      
+
+		    if(this.z < 0)        
+            this.z = arenaZSize;      
+                                                           
+        
+        this.draw()                           
+    }
+
+}
+
+
+document.addEventListener('keydown', function(event) {
+
+    if(event.code == 'ShiftLeft'){}	
+        //Player z down
+    
+    if(event.code == 'Space'){}	  
+        //Player z up
+    
+
+    if(event.code == 'KeyW'){}	       
+        //Player x forward
+    
+
+    if(event.code == 'KeyS'){}	
+		//Player x backward
+    
+	if(event.code == 'KeyA'){}	         
+        //Player y left
+
+	if(event.code == 'KeyD'){}	          
+        //Player y right
+        
+})
+
+
+function Player(x, y, z, dx, dy, dz, health, mana) {
+
+
+}
+
+
+
